016d45e8694cffd4d330720347e9ec6b40d97a15
MohammadAminPiri <mohammadaminpiri2@gmail.com>
2024-12-23 21:07:17 +0100

    nested

---
+++ nestedLoops.py
@@ -0,0 +1,6 @@
+# Write a program to print the multiplication table (1 to 10) for numbers from 1 to 5 using nested loops.
+
+
+for i in range(1, 6):
+    for n in range(1, 11):
+        print(i * n)
